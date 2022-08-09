5df62135bcfc44882abb13c63104c09141b09823
Gennadii Plahonin <plahonin99@mail.ru>
2022-08-09 10:06:12 +0900

    добавлен rollback() для отката изменений, запросы перенесены в класс UserDaoJDVCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,27 +9,52 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     private static final Connection connection = Util.getConnection();
+
+    // SQL-запросы
+    static final String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS User ( " +
+            "id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT, " +
+            "name VARCHAR(25), " +
+            "last_name VARCHAR(25), " +
+            "age TINYINT UNSIGNED )";
+
+    static final String DROP_USER_TABLE = "DROP TABLE IF EXISTS User";
+    static final String ADD_NEW_USER = "INSERT INTO user(name, last_name, age) VALUES (?, ?, ?)";
+    static final String REMOVE_USER_BY_ID = "DELETE FROM user WHERE id = ?";
+    static final String SELECT_ALL_USERS = "SELECT * FROM user";
+    static final String CLEAN_USERS_TABLE = "DELETE FROM user";
+    
     public UserDaoJDBCImpl() {
         // Пустой конструктор
     }
 
     public void createUsersTable() {
         // Создание таблицы User
-        Statement statement = null;
-        executeQuery(QueriesSQL.CREATE_USER_TABLE);
+        try {
+            executeQuery(CREATE_USER_TABLE);
+        }
+        catch (SQLException ignore) {
+            System.out.println("Произошла ошибка при создании таблицы User. Изменения отменены.");
+        }
     }
 
     public void dropUsersTable() {
         // Удаление таблицы User
-        executeQuery(QueriesSQL.DROP_USER_TABLE);
+        try {
+            executeQuery(DROP_USER_TABLE);
+        }
+        catch (SQLException ignore) {
+            System.out.println("Произошла ошибка при удалении таблицы User. Изменения отменены.");
+        }
     }
 
-    public void executeQuery(String query) {
+    public void executeQuery(String query) throws SQLException {
         try {
             Statement statement = connection.createStatement();
             statement.execute(query);
             connection.commit();
         } catch (SQLException e) {
+            // Откатываем изменения
+            connection.rollback();
             throw new RuntimeException(e);
         }
     }
@@ -38,13 +63,19 @@ public class UserDaoJDBCImpl implements UserDao {
         // Добавление нового пользователя
         PreparedStatement statement;
         try {
-            statement = connection.prepareStatement(QueriesSQL.ADD_NEW_USER);
+            statement = connection.prepareStatement(ADD_NEW_USER);
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
             statement.execute();
             connection.commit();
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            }
+            catch (SQLException ignore) {
+                System.out.println("При попытке добавления пользователя произошла ошибка. Изменения отменены.");
+            }
             throw new RuntimeException(e);
         }
     }
@@ -53,12 +84,18 @@ public class UserDaoJDBCImpl implements UserDao {
         // Удаление пользователя по id
         PreparedStatement statement;
         try {
-            statement = connection.prepareStatement(QueriesSQL.REMOVE_USER_BY_ID);
+            statement = connection.prepareStatement(REMOVE_USER_BY_ID);
             statement.setLong(1, id);
             statement.execute();
             connection.commit();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+        try {
+            connection.rollback();
+        }
+        catch (SQLException ignore) {
+            System.out.println("При попытке удаления пользователя произошла ошибка. Изменения отменены.");
         }
     }
 
@@ -69,11 +106,17 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> users = new ArrayList<>();
         try {
             statement = connection.createStatement();
-            data = statement.executeQuery(QueriesSQL.SELECT_ALL_USERS);
+            data = statement.executeQuery(SELECT_ALL_USERS);
             while (data.next()) {
                 users.add(new User(data.getString(2), data.getString(3), data.getByte(4)));
             }
         } catch (SQLException e) {
+            try {
+                connection.rollback();
+            }
+            catch (SQLException ignore) {
+                System.out.println("При попытке получения списка пользователей произошла ошибка. Изменения отменены.");
+            }
             throw new RuntimeException(e);
         }
         return users;
@@ -81,13 +124,11 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         // Очистка таблицы User
-        Statement statement;
         try {
-            statement = connection.createStatement();
-            statement.execute(QueriesSQL.CLEAN_USERS_TABLE);
-            connection.commit();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            executeQuery(CLEAN_USERS_TABLE);
+        }
+        catch (SQLException ignore) {
+            System.out.println("Произошла ошибка при очистки таблицы User. Изменения отменены.");
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -17,7 +17,7 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoObj.saveUser(name, lastName, age);
-        System.out.printf("User с именем – %s добавлен в базу данных", name);
+        System.out.printf("User с именем – %s добавлен в базу данных\n", name);
     }
 
     public void removeUserById(long id) {
